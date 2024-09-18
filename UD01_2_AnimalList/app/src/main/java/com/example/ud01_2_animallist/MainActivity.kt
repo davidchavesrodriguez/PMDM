a1cc35c1c8dd90893b9ea520f1d30ebe9e02451e
A22DavidCR <A22DavidCR@iessanclemente.net>
2024-09-18 13:43:31 +0200

    Animals array

--- UD01_2_AnimalList/app/src/main/java/com/example/ud01_2_animallist/MainActivity.kt
+++ UD01_2_AnimalList/app/src/main/java/com/example/ud01_2_animallist/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.ud01_2_animallist
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val sendButton= findViewById<Button>(R.id.sendButton)
+        // Vamos a añadir un click listener
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
