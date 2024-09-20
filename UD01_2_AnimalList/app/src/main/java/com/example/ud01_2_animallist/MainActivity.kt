5879316a0f56f9d01fd460f3c4f9dc9c34c0ff59
A22DavidCR <A22DavidCR@iessanclemente.net>
2024-09-20 14:33:06 +0200

    Breed selector done. Chronos started

--- UD01_2_AnimalList/app/src/main/java/com/example/ud01_2_animallist/MainActivity.kt
+++ UD01_2_AnimalList/app/src/main/java/com/example/ud01_2_animallist/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.ud01_2_animallist
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,12 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val sendButton= findViewById<Button>(R.id.sendButton)
-        // Vamos a añadir un click listener
+
+        val buttonSend = findViewById<Button>(R.id.buttonSend)
+        buttonSend.setOnClickListener {
+            val spinnerAnimalType = findViewById<Spinner>(R.id.listAnimals)
+            val textAnimalKind = findViewById<TextView>(R.id.textAnimalKind)
+            textAnimalKind.text = getAnimalKind(spinnerAnimalType.selectedItemId).joinToString("\n")
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun getAnimalKind(id: Long): List<String> = when (id) {
+        0L -> resources.getStringArray(R.array.dogBreeds).toList()
+        1L -> resources.getStringArray(R.array.catBreeds).toList()
+        2L -> resources.getStringArray(R.array.duckBreeds).toList()
+        else -> listOf()
     }
 }
