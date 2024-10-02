acf8264e2989b278bb37e7ca1d56c0ab35aa1ccd
A22DavidCR <A22DavidCR@iessanclemente.net>
2024-10-02 13:43:06 +0200

    Linear done

--- UD02_5_FrameLayoutPractice/app/src/main/java/com/example/ud02_5_framelayoutpractice/MainActivity.kt
+++ UD02_5_FrameLayoutPractice/app/src/main/java/com/example/ud02_5_framelayoutpractice/MainActivity.kt
@@ -1,8 +1,6 @@
-package com.example.ud02_4_linearlayoutpractice
+package com.example.ud02_5_framelayoutpractice
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,15 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        val buttonCall = findViewById<Button>(R.id.buttonCall)
-        buttonCall.setOnClickListener {
-            val paragraphCalling = findViewById<TextView>(R.id.textCalling)
-            val contactName = findViewById<TextView>(R.id.textContact)
-            val textCalling = getString(R.string.textCalling) + " " + contactName.text
-            paragraphCalling.text = textCalling
-        }
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
