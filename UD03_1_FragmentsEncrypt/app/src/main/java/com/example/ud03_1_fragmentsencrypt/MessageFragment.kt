63d4932ddf54938a667f932ab755a3fc5f506b63
A22DavidCR <A22DavidCR@iessanclemente.net>
2024-10-04 14:28:45 +0200

    Fragments started

---
+++ UD03_1_FragmentsEncrypt/app/src/main/java/com/example/ud03_1_fragmentsencrypt/MessageFragment.kt
@@ -0,0 +1,21 @@
+package com.example.ud03_1_fragmentsencrypt
+
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MessageFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflar el diseño desde el fragmento
+        return inflater.inflate(R.layout.fragment_message, container, false)
+    }
+
+}
