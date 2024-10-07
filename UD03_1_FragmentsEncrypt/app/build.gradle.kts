7569236d57c51d27e3a6a22c8394602169ef37d1
A22DavidCR <A22DavidCR@iessanclemente.net>
2024-10-07 14:34:27 +0200

    More Fragments

--- UD03_1_FragmentsEncrypt/app/build.gradle.kts
+++ UD03_1_FragmentsEncrypt/app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation("androidx.fragment:fragment-ktx:1.8.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- UD03_1_FragmentsEncrypt/app/src/main/java/com/example/ud03_1_fragmentsencrypt/WelcomeFragment.kt
+++ UD03_1_FragmentsEncrypt/app/src/main/java/com/example/ud03_1_fragmentsencrypt/WelcomeFragment.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class WelcomeFragment : Fragment() {
     override fun onCreateView(
@@ -14,8 +16,13 @@ class WelcomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflar el diseño desde el fragmento
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
-    }
 
+        // Inflar el diseño desde el fragmento
+        var view= inflater.inflate(R.layout.fragment_welcome, container, false)
+        val buttonStart= view.findViewById<Button>(R.id.button_start)
+        buttonStart.setOnClickListener {
+            view.findNavController().navigate(R.id.action_welcomeFragment_to_messageFragment)
+        }
+        return view
+    }
 }
