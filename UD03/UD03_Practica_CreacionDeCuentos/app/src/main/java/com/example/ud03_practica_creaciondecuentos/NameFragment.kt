0e675b09f90a72e4bf3f352d9e11fb9e955c457c
A22DavidCR <A22DavidCR@iessanclemente.net>
2024-10-18 14:34:36 +0200

    pmdm is hard

--- UD03/UD03_Practica_CreacionDeCuentos/app/src/main/java/com/example/ud03_practica_creaciondecuentos/NameFragment.kt
+++ UD03/UD03_Practica_CreacionDeCuentos/app/src/main/java/com/example/ud03_practica_creaciondecuentos/NameFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.navigation.findNavController
 import com.example.ud03_practica_creaciondecuentos.R
 
@@ -18,10 +19,15 @@ class NameFragment : Fragment() {
         // Inflate fragment
         val view = inflater.inflate(R.layout.fragment_name, container, false)
 
+        // EditText
+        val editTextName = view.findViewById<EditText>(R.id.main_name)
+
         // Button
         val buttonStart = view.findViewById<Button>(R.id.main_next_button)
         buttonStart.setOnClickListener {
-            view.findNavController().navigate(R.id.action_nameFragment_to_themeFragment)
+            val name = editTextName.text.toString()
+            val action = NameFragmentDirections.actionNameFragmentToThemeFragment(name)
+            view.findNavController().navigate(action)
         }
 
         return view
